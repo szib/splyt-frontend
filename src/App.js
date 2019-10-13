4f1687c6bba3d7e4036b35ed53f549a8e9470c11
Ivan Szebenszki <szib77@gmail.com>
2019-10-13 20:31:24 +0100

    Add material UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
 
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
 import TaxiMap from "./components/Map/TaxiMap";
 
 import "./App.css";
 
+const useStyles = makeStyles(theme => ({
+  header: {
+    margin: theme.spacing(6, 0, 3),
+    textAlign: "center"
+  }
+}));
+
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <h1>Splyt Taxi Map</h1>
+    <div>
+      <Typography variant="h2" className={classes.header}>
+        Splyt Taxi Map
+      </Typography>
       <TaxiMap />
     </div>
   );
